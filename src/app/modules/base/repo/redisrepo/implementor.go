74df0b566fa18710a2b070c4a33331a2f24f1f09
Okto Prima <octoprima93@gmail.com>
2025-02-19 15:31:06 +0700

    Redis integration on repository

--- src/app/modules/base/repo/redisrepo/implementor.go
+++ src/app/modules/base/repo/redisrepo/implementor.go
@@ -2,6 +2,7 @@ package redisrepo
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/gomodule/redigo/redis"
 	"github.com/oktopriima/marvel/pkg/cache"
 	"github.com/oktopriima/marvel/src/app/modules/base/model"
@@ -18,22 +19,74 @@ func NewBaseRedisRepo(instance cache.RedisInstance) *BaseRedisRepo {
 	}
 }
 
-func (r *BaseRedisRepo) FindCache(ctx context.Context, key string) (model.Model, error) {
-	return nil, nil
+func (r *BaseRedisRepo) FindCache(ctx context.Context, m model.Model, key string) (model.Model, error) {
+	conn, err := r.redis.GetContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	bytes, err := redis.Bytes(conn.Do("GET", key))
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(bytes, &m)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
 }
 
 func (r *BaseRedisRepo) FindRawCache(ctx context.Context, key string) ([]byte, error) {
-	return nil, nil
+	conn, err := r.redis.GetContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	bytes, err := redis.Bytes(conn.Do("GET", key))
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
 }
 
 func (r *BaseRedisRepo) StoreCache(ctx context.Context, key string, ttl time.Duration, m model.Model) error {
-	return nil
+	conn, err := r.redis.GetContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	_, err = conn.Do("SETEX", key, ttl, m)
+	return err
 }
 
 func (r *BaseRedisRepo) StoreObjectCache(ctx context.Context, key string, ttl time.Duration, m []byte) error {
-	return nil
+	conn, err := r.redis.GetContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	_, err = conn.Do("SETEX", key, ttl, m)
+	return err
 }
 
 func (r *BaseRedisRepo) RemoveCache(ctx context.Context, key string) error {
-	return nil
+	conn, err := r.redis.GetContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+	_, err = conn.Do("DEL", key)
+	return err
 }
